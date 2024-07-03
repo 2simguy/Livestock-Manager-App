457b13fca4943543d45bd96b301690732cea65b3
2simguy <42773827+2simguy@users.noreply.github.com>
2024-07-03 11:26:14 +0200

    cleaned up initial build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'livestock.manager.app',
   appName: 'Livestock Manager App',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
