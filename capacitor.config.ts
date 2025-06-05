f4841358ea4060a20566721710e8ce24475dc664
2simguy <42773827+2simguy@users.noreply.github.com>
2025-06-05 10:55:38 +0200

    clean slate

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'livestock.manager.app',
-  appName: 'Livestock Manager App',
-  webDir: 'www/browser'
-};
-
-export default config;
